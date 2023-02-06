c655ea015612acad4b028c5342dbb9fec4ce504d
Yusuf Dumlu <yusuf.ex@gmail.com>
2023-02-06 04:35:25 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+const text = document.getElementById("text");
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    text.innerHTML = "This is really cool!";
+});
